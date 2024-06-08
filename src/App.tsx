f805133fce5baa73ff756bad75a53a8836083480
yuta-takeda <ytakeda@jmdc.co.jp>
2024-06-08 14:25:19 +0900

    create UI container

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="p-8 w-full max-w-screen-md bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold text-center">Generate your post</h1>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
