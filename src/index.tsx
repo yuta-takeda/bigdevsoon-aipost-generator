3a93fc1e2dda5cbf0c34c7cbe7e40c5c77bc0132
yuta-takeda <ytakeda@jmdc.co.jp>
2024-06-08 21:07:40 +0900

    import react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Routes, Route, Outlet, BrowserRouter } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Outlet />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
